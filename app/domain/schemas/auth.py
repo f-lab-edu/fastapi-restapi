c8ea632438a93d814b7467ad5b7e7c9682147b62
kyungsoo Jung <wjdrudtn97@naver.com>
2024-09-01 12:04:55 +0900

    안쓰는 코드/주석 삭제

--- app/domain/schemas/auth.py
+++
@@ -1,7 +0,0 @@
-# app/domain/schemas/auth.py
-from pydantic import BaseModel
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
--- app/main.py
+++ app/main.py
@@ -10,17 +10,7 @@ from app.database import Base, engine
 
 app = FastAPI()
 
-
-# app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(api_router, prefix="/api", tags=["api"])
-# 인증 및 보호된 라우터 등록
-# api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
-# app.include_router(protected.router, prefix="/protected", tags=["protected"])
-
-# main 실행 코드
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 # 기존 테이블 삭제
 Base.metadata.drop_all(bind=engine)
